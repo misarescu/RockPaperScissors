ae8398098f3677845dcac0ae8653d89b0dfbc932
redshadowfx <redshadowfx60@gmail.com>
2023-05-04 22:09:40 +0300

    updated the root layout styling and corrected the tailwind text styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
       colors:{
         'primary' : '#e2e8f0', // slate-200
         'primary-dark': '#1e293b', // slate-800
-        'primary-text' : '#e2e8f0', // slate-200
-        'primary-text-dark' : '#1e293b', // slate-800
+        'primary-text' : '#1e293b', // slate-200
+        'primary-text-dark' : '#e2e8f0', // slate-800
         'secondary': '#64748b', // slate-500
         'accent' : '#38bdf8' // sky-400
       }

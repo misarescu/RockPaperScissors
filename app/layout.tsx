a2a070a78ec63995f8acabcb55061621edf30dce
Isarescu Mihai <mihai.isarescu@zohomail.eu>
2023-05-08 00:09:19 +0300

    improved the styles and made the layout better

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,27 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Rock Paper Scissors',
-  description: 'Take your rock paper scissors game to the next level'
+  description: 'Take your rock paper scissors game to the next level',
 };
 
+const navText = 'Play the best version of 🪨 🧻 ✂️';
+
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-primary dark:bg-primary-dark text-primary-text dark:text-primary-text-dark dark:text-primary-dark">
-        <nav className="flex flex-row justify-center w-full">
-          <div>Play the best version of rock paper scissors</div>
+    <html lang='en'>
+      <body className='bg-primary dark:bg-primary-dark text-primary-text dark:text-primary-text-dark text-5xl font-black font-sans'>
+        <nav className='flex flex-row justify-center items-center w-full pt-2 pb-2 mb-2 bg-secondary drop-shadow-accent-bold dark:drop-shadow-accent-light'>
+          <p className='drop-shadow-accent-light hover:drop-shadow-accent-bold text-accent'>
+            {navText}
+          </p>
         </nav>
-        {children}
+        <main className='flex flex-col items-center justify-center h-screen'>
+          {children}
+        </main>
       </body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        'primary' : '#e2e8f0', // slate-200
+      colors: {
+        primary: '#e2e8f0', // slate-200
         'primary-dark': '#1e293b', // slate-800
-        'primary-text' : '#1e293b', // slate-200
-        'primary-text-dark' : '#e2e8f0', // slate-800
-        'secondary': '#64748b', // slate-500
-        'accent' : '#38bdf8' // sky-400
-      }
+        'primary-text': '#1e293b', // slate-200
+        'primary-text-dark': '#e2e8f0', // slate-800
+        secondary: '#64748b', // slate-500
+        accent: '#38bdf8', // sky-400
+      },
+    },
+    dropShadow: {
+      // add drop shadows with weights and accent color of the theme
+      'accent-light': '0px 0px 15px rgba(56, 189, 248, 0.3)',
+      'accent-bold': '0px 0px 20px rgba(56, 189, 248, 0.7)',
     },
   },
   plugins: [],
-}
+};

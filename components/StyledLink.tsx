b3eee99b739c927d2902353bc6107813ab66c187
redshadowfx <mihai.isarescu@protonmail.com>
2023-05-11 01:43:27 +0300

    added transition effects to the links

--- components/StyledLink.tsx
+++ components/StyledLink.tsx
@@ -9,7 +9,7 @@ function StyledLink(props: Props) {
   return (
     <Link
       href={props.href}
-      className="bg-secondary text-accent py-4 px-8 m-8 hover:py-6 hover:px-10 hover:m-6 drop-shadow-accent-light dark:drop-shadow-accent-bold rounded-md"
+      className="bg-secondary text-accent py-4 px-8 m-8 drop-shadow-accent-light dark:drop-shadow-accent-bold rounded-md transition ease-in-out duration-150 hover:scale-110"
     >
       {props.children}
     </Link>

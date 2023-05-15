86f300f31e65ea82b80a1f03289466eb6c03cad1
redshadowfx <mihai.isarescu@protonmail.com>
2023-05-15 14:49:55 +0300

    fixed a small bug where the room name got encoded in the URL and it was showed wierdly in the room page

--- app/game/room/[id]/[name]/[playerLimit]/page.tsx
+++ app/game/room/[id]/[name]/[playerLimit]/page.tsx
@@ -7,6 +7,7 @@ type Props = {
 };
 
 function GameRoom({ params }: Props) {
+  params.name = decodeURI(params.name); // need to decode in case the room name contains encoded characters
   return (
     <>
       <div>GameRoom no: {params.id}</div>
